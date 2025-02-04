bf929e4544da9e889def54c671450674088a162c
Niva Phueklieng <dphuekli@uci.edu>
2025-02-03 20:21:52 -0800

    background, banner, frame

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -55,6 +55,17 @@ SKILLS SECTION (Einar)
 
 const Profile = () => {
 
-  }
+  return (
+    <div className="absolute overflow-hidden top-10 h-full w-full bg-gradient-to-b from-gray-100 to-gray-200">
+      <div className="absolute top-10 left-52 h-full w-3/5 bg-white rounded-xl border border-solid border-gray-300">
+      </div>
+
+	  {/* ----- BANNER ----- */}
+	  <img src="https://static.vecteezy.com/system/resources/thumbnails/054/506/874/small/lush-forest-floor-with-ferns-and-sunlight-free-photo.jpg" className="absolute top-10 left-52 h-2/5 w-3/5 bg-emerald-900 rounded-t-xl border border-solid border-gray-300"/>
+	  
+    </div>
+  )
+
+}
 
 export default Profile
