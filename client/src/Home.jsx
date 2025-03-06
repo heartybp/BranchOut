e9f4b49e4459901d0e8182686f663e997030e6ec
heartybp <parrenashearty@gmail.com>
2025-03-05 23:58:52 -0800

    draft of post component

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,10 +2,29 @@ import React from 'react'
 import Navbar from './components/Navbar.jsx'
 
 const Home = () => {
+
   return (
     <>
       <Navbar />
-      <div>This is the homepage</div>
+      
+      <div className=" bg-green-700 flex justify-center items-center">
+
+        {/* post ui component */}
+        <div className="w-1/2 h-20 flex-col bg-blue-200 rounded-md shadow-sm shadow-gray-500">
+          
+          <div className="flex justify-center">
+            <div>profile photo holder</div>
+            <h3>@username</h3>
+          </div>
+
+          {/* text content of post */}
+          <div className='flex justify-center border-gray-200 border-2 w-10/12 bg-green-200'>
+            <p>lorem ipsum dolor</p>
+          </div>
+          
+        </div>
+
+      </div>
     </>
   )
 }
