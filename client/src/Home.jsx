04695d2444e348d53603fcd0f65dd5ba309b5f01
heartybp <parrenashearty@gmail.com>
2025-03-11 03:16:45 -0700

    small styling update

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -10,15 +10,15 @@ const Home = () => {
       <div className=" bg-green-700 flex justify-center items-center">
 
         {/* post ui component */}
-        <div className="w-1/2 h-20 flex-col bg-blue-200 rounded-md shadow-sm shadow-gray-500">
+        <div className="w-1/3 h-20 flex flex-col justify-center items-center bg-white rounded-md shadow-sm shadow-gray-500">
           
-          <div className="flex justify-center">
+          <div className="flex">
             <div>profile photo holder</div>
             <h3>@username</h3>
           </div>
 
           {/* text content of post */}
-          <div className='flex justify-center border-gray-200 border-2 w-10/12 bg-green-200'>
+          <div className='w-10/12 bg-white rounded-md  border-2 '>
             <p>lorem ipsum dolor</p>
           </div>
           
