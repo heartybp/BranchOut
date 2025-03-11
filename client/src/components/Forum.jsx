595e2206b5879c7562ac205c5113bd8e0c344107
Niva Phueklieng <dphuekli@uci.edu>
2025-03-10 23:03:46 -0700

    fix handle functions

--- client/src/components/Forum.jsx
+++ client/src/components/Forum.jsx
@@ -7,7 +7,34 @@ const Forum = () => {
   const [showCommentPopup, setShowCommentPopup] = useState(false);
   const [selectedQuestion, setSelectedQuestion] = useState(null);
   const [comment, setComment] = useState('');
+  
+  const handleSubmitQuestion = () => {
+    if (questionTitle && questionBody) {
+      const newQuestion = {
+        title: questionTitle,
+        body: questionBody,
+        comments: []
+      };
+      setQuestions([newQuestion, ...questions]);
+      setQuestionTitle('');
+      setQuestionBody('');
+    }
+  };
 
+  const handleCommentClick = (question) => {
+    setSelectedQuestion(question);
+    setShowCommentPopup(true);
+  };
+
+  const handleAddComment = () => {
+    if (comment) {
+      selectedQuestion.comments.push(comment);
+      setQuestions([...questions]);
+      setComment('');
+    }
+  };
+
+  
 };
 
 export default Forum;
