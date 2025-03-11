9476ab5b5fc31d1373ac6372231006642c8b7410
Niva Phueklieng <dphuekli@uci.edu>
2025-03-10 22:55:23 -0700

    reinforce forum logic

--- client/src/components/Forum.jsx
+++ client/src/components/Forum.jsx
@@ -1,30 +1,7 @@
-import React from "react";
-
+import React, { useState } from 'react';
 
 const Forum = () => {
-    const [title, setTitle] = React.useState("");
-    const [body, setBody] = React.useState("");
 
-    return (
-        <div className="max-w-4xl mx-auto p-4 space-y-6">
-            <div className="max-w-4xl mx-auto p-4 space-y-6">
-                <input 
-                    placeholder="Question Title" 
-                    value={title}
-                    onChange={(event) => setTitle(event.target.value)}
-                    className="w-full p-1"
-                />
-                <textarea
-                    placeholder="Type your question here..."
-                    value={body}
-                    onChange={(event) => setBody(event.target.value)}
-                    className="w-full p-1"
-                />
-                <button
-                />
-            </div>
-        </div>
-    );
-}
+};
 
-export default Forum
+export default Forum;
