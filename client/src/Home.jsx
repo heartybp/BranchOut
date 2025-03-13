404c8ab11fc717c69b9370913260e519803917f6
heartybp <parrenashearty@gmail.com>
2025-03-12 23:59:24 -0700

    updated post ui and separate into component

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
-import Navbar from './components/Navbar.jsx'
+import React from 'react';
+import Navbar from './components/Navbar.jsx';
+import Post from './components/Post.jsx';
 
 const Home = () => {
+  // Example posts data - in a real app, this might come from an API
+  const posts = [
+    {
+      id: 1,
+      username: 'johndoe',
+      content: 'This is my first post!',
+      profilePhoto: null
+    },
+    {
+      id: 2,
+      username: 'janedoe',
+      content: 'React is amazing!',
+      profilePhoto: null
+    }
+  ];
 
   return (
     <>
       <Navbar />
-      
-      <div className=" bg-green-700 flex justify-center items-center">
-
-        {/* post ui component */}
-        <div className="w-1/3 h-20 flex flex-col justify-center items-center bg-white rounded-md shadow-sm shadow-gray-500">
-          
-          <div className="flex">
-            <div>profile photo holder</div>
-            <h3>@username</h3>
-          </div>
-
-          {/* text content of post */}
-          <div className='w-10/12 bg-white rounded-md  border-2 '>
-            <p>lorem ipsum dolor</p>
-          </div>
-          
+      <div className="bg-green-700 flex justify-center items-center py-8">
+        {/* Container for posts */}
+        <div className="w-full max-w-2xl space-y-4">
+          {posts.map(post => (
+            <Post 
+              key={post.id}
+              username={post.username}
+              content={post.content}
+              profilePhoto={post.profilePhoto}
+            />
+          ))}
         </div>
-
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
