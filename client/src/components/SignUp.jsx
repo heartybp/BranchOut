a96a5118aa9b37278998de79c5792b59c8fe6bed
heartybp <parrenashearty@gmail.com>
2025-03-23 22:30:54 -0700

    update signup/login icon spacing and navbar

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import {MailOutlined, DiscordOutlined, GoogleOutlined, 
     UserOutlined, LockOutlined, LockFilled, QuestionCircleFilled, MailFilled} from '@ant-design/icons';
-import { Leaf } from 'lucide-react';
+import { Leaf, School } from 'lucide-react';
 
 
 const SignUp = () => {
@@ -22,7 +22,7 @@ const SignUp = () => {
         {/* image component */}
         <div className="relative w-3/5 bg-emerald-800">
             <img class="absolute w-full h-full object-cover" src="/src/assets/login-cover.jpg" />
-            <div className="absolute inset-0 bg-emerald-800 bg-opacity-40"></div>
+            <div className="absolute inset-0 bg-brandGreen bg-opacity-40"></div>
         </div>
 
         <div className="w-2/5 bg-lightGreen h-screen">
@@ -30,12 +30,12 @@ const SignUp = () => {
                 <div className="flex flex-col w-full">
 
                     <div className='flex items-center justify-center'>
-                        <h1 className='text-emerald-900 font-bold text-4xl mt-20 mb-8'>BranchOut</h1>
-                        <Leaf size={50} className='text-emerald-600 relative left-4 top-5'/>
+                        <h1 className='text-brandGreen font-bold text-4xl mt-20 mb-8'>BranchOut</h1>
+                        <Leaf size={50} className='text-mediumGreen relative left-4 top-5'/>
                     </div>
                     
 
-                    <div className="flex flex-col text-emerald-700 items-center justify-center space-y-1">
+                    <div className="flex flex-col text-mediumGreen items-center justify-center space-y-1">
                         <h1 className="text-2xl font-semibold flex justify-center">Create an Account</h1>
                         <p>Fill out your information</p>
                     </div>
@@ -43,22 +43,22 @@ const SignUp = () => {
                     <div className="w-4/5 mx-auto my-8 px-4">
                         <div className="flex items-center w-full">
                             <div className="flex-grow h-px bg-emerald-600"></div>
-                            <span className="px-4 text-emerald-600 text-sm font-light">or continue with</span>
+                            <span className="px-4 text-mediumGreen text-sm font-light">or continue with</span>
                             <div className="flex-grow h-px bg-emerald-600"></div>
                         </div>
                     </div>
                     
-                    <div className="flex justify-center space-x-4 mb-8">
+                    <div className="flex justify-center space-x-4 mb-8 text-mediumGreen">
                         <button className="flex items-center justify-center rounded-full w-10 h-10 bg-white shadow-sm">
-                            <MailOutlined className="text-emerald-700 text-xl"/>
+                            <School className="text-xl"/>
                         </button>
 
                         <button className="flex items-center justify-center rounded-full w-10 h-10 bg-white shadow-sm">
-                            <GoogleOutlined className="text-emerald-700 text-xl"/>
+                            <GoogleOutlined className="text-xl"/>
                         </button>
 
                         <button className="flex items-center justify-center rounded-full w-10 h-10 bg-white shadow-sm">
-                            <DiscordOutlined className="text-emerald-700 text-2xl"/>
+                            <DiscordOutlined className="text-2xl"/>
                         </button>
 
                     </div>
@@ -75,7 +75,7 @@ const SignUp = () => {
                                 className="w-11/12 p-2 pl-9 mr-1 rounded-md mb-4 border border-gray-300 outline-none"
                             />
 
-                            <UserOutlined className="absolute left-60 top-2.5 text-xl text-gray-400"/>
+                            <UserOutlined className="absolute pl-2 left-60 top-2.5 text-xl text-gray-400"/>
                             <input 
                                 type="text" 
                                 id="lastName" 
@@ -85,27 +85,27 @@ const SignUp = () => {
                         </div>
 
                         <div className="w-full relative flex items-center justify-center">
-                            <MailFilled className="absolute left-16 top-2.5 text-xl text-gray-400"/>
+                            <MailFilled className="absolute pl-2 left-16 top-2.5 text-xl text-gray-400"/>
                             <input 
                                 type="email" 
                                 id="email" 
                                 placeholder="Email"
-                                className="w-4/5 p-2 pl-9 rounded-md mb-4 border border-gray-300 outline-none"
+                                className="w-4/5 p-2 pl-10 rounded-md mb-4 border border-gray-300 outline-none"
                             />
                         </div>
 
                         <div className="w-full relative flex items-center justify-center">
-                            <LockOutlined className="absolute left-16 top-2.5 text-xl text-gray-400"/>
+                            <LockOutlined className="absolute pl-2 left-16 top-2.5 text-xl text-gray-400"/>
                             <input 
                                 type="password" 
                                 id="password" 
                                 placeholder="Password"
-                                className="w-4/5 p-2 pl-9 rounded-md mb-4 border border-gray-300 outline-none"
+                                className="w-4/5 p-2 pl-10 rounded-md mb-4 border border-gray-300 outline-none"
                             />
                         </div>
 
                         <div className="w-full relative flex items-center justify-center">
-                            <LockFilled className="absolute left-16 top-2.5 text-xl text-gray-400"/>
+                            <LockFilled className="absolute pl-2 left-16 top-2.5 text-xl text-gray-400"/>
                             <input 
                                 type="password" 
                                 id="passwordConfirm" 
@@ -116,7 +116,7 @@ const SignUp = () => {
 
                         <div className="w-full flex justify-center">
                             <button 
-                                className="w-4/5 my-2 p-2 bg-emerald-700 text-white rounded-md hover:bg-emerald-900"
+                                className="w-4/5 my-2 p-2 bg-brandGreen text-white rounded-md hover:bg-mediumGreen"
                                 onClick={navigateHome}
                             >
                                 Continue
@@ -126,12 +126,12 @@ const SignUp = () => {
                         
                     </form>
 
-                    <div className='flex flex-col items-center mt-4 text-sm text-emerald-700'>
+                    <div className='flex flex-col items-center mt-4 text-sm text-mediumGreen'>
                         <p className="cursor-pointer">Forgot Password</p>
                         <p className="cursor-pointer" onClick={navigateLogIn}>Already have an account</p>
 
 
-                        <div className="flex flex-row items-center justify-center text-gray-400 mt-10">
+                        <div className="flex flex-row items-center justify-center text-gray-500 mt-10">
                             <QuestionCircleFilled/>
                             <p className="pl-2">Need help or support</p>
                         </div>
