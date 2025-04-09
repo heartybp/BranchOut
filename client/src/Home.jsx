adb343ff560b4a6367970e6dec066f5285a83b6f
heartybp <parrenashearty@gmail.com>
2025-04-08 23:37:54 -0700

    fixed scrollable container, styled home cards

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -7,99 +7,113 @@ import SuggestedConnections from "./components/SuggestedConnections.jsx";
 import SeeMoreButton from "./components/SeeMoreButton.jsx";
 import Header from "./components/Header.jsx";
 import Assignments from "./components/Assignments.jsx";
-
+import mentorImage from "./assets/mentor.png";
 
 const Home = () => {
-
   const toCompleteAssignments = [
-    { 
-      resource: "Read Chapter 5", 
-      assignedBy: "John Smith", 
-      assignedDate: "04/02", 
-      dueIn: "3 days" 
+    {
+      resource: "Read Chapter 5",
+      assignedBy: "John Smith",
+      assignedDate: "04/02",
+      dueIn: "3 days",
     },
-    { 
-      resource: "Complete Quiz", 
-      assignedBy: "Jane Doe", 
-      assignedDate: "04/01", 
-      dueIn: "2 days" 
-    }
+    {
+      resource: "Complete Quiz",
+      assignedBy: "Jane Doe",
+      assignedDate: "04/01",
+      dueIn: "2 days",
+    },
   ];
 
   const upcomingAssignments = [
-    { 
-      resource: "Read Chapter 6", 
-      assignedBy: "John Smith", 
-      assignedDate: "04/08", 
-      dueIn: "9 days" 
+    {
+      resource: "Read Chapter 6",
+      assignedBy: "John Smith",
+      assignedDate: "04/08",
+      dueIn: "9 days",
     },
-    { 
-      resource: "Written Homework", 
-      assignedBy: "Jane Doe", 
-      assignedDate: "04/05", 
-      dueIn: "6 days" 
-    }
+    {
+      resource: "Written Homework",
+      assignedBy: "Jane Doe",
+      assignedDate: "04/05",
+      dueIn: "6 days",
+    },
   ];
 
   return (
-    <div className="flex max-h-screen">
-      <Navbar />
+    <>
+      <div className="flex max-h-screen">
+        <Navbar />
 
-      {/* left side */}
-      <div className="bg-white justify-center w-[940px] flex-row">
-        <Header />
-        {/* tree, checked in, + mentor */}
-        <div className="flex bg-blue-200">
-          <div>
-            <img src={Tree} alt="" width={500} />
-          </div>
-          
-          <div className="flex flex-col ">
-            <div className="flex flex-col bg-gray-200 p-16">
-              <p>20 days</p>
-              <p>checked in</p>
+        <div className="flex flex-col overflow-y-scroll bg-white">
+          <Header />
+
+          <div className="flex bg-white">
+            {/* left side */}
+
+            <div className="bg-white justify-center w-[940px] flex-row ">
+              {/* tree, checked in, + mentor */}
+              <div className="flex justify-center gap-x-5 bg-blue-200">
+                {/* jane's tree */}
+                <div>
+                  <img src={Tree} alt="" width={500} />
+                </div>
+
+                <div className="flex flex-col justify-between w-72">
+                  <div className="flex flex-col bg-gray-100 p-9 rounded-lg border border-gray-300">
+                    <p className="text-3xl font-semibold">20</p>
+                    <p className="font-semibold">days checked in</p>
+                  </div>
+
+                  <div className="bg-gray-100 p-6 rounded-lg border border-gray-300 flex flex-col items-center">
+                    <p className="font-semibold">Current Mentor</p>
+                    <img
+                      src={mentorImage}
+                      alt=""
+                      width={100}
+                      className="my-2"
+                    />
+                    <p className="font-semibold">Josh Doe</p>
+                  </div>
+                </div>
+              </div>
+
+              <ScrollEvents ScrollEventsTitle="Ongoing Mentor Matchings" />
+              <SeeMoreButton />
+
+              <ScrollEvents ScrollEventsTitle="Upcoming Mentor Matchings" />
+              <SeeMoreButton />
+
+              <SuggestedConnections />
+              <SeeMoreButton />
             </div>
 
-            <div className="bg-gray-400 p-16">
-              <p>current mentor: josh doe</p>
+            {/* right side */}
+            <div className="bg-white flex justify-center">
+              <div className="flex flex-col bg-gray-100 rounded-md border border-gray-300 w-[390px] mx-6">
+                <h2>This Week:</h2>
+                <p className="rounded-3xl w-36 h-48 bg-blue-200">
+                  placeholder for progress bar
+                </p>
+
+                {/* component */}
+                <Assignments
+                  title="Complete"
+                  assignments={toCompleteAssignments}
+                />
+
+                <Assignments
+                  title="Upcoming"
+                  assignments={upcomingAssignments}
+                />
+
+                <SeeMoreButton />
+              </div>
             </div>
           </div>
         </div>
-        
-        <ScrollEvents ScrollEventsTitle="Ongoing Mentor Matchings" />
-        <SeeMoreButton />
-
-        <ScrollEvents ScrollEventsTitle="Upcoming Mentor Matchings" />
-        <SeeMoreButton />
-
-        <SuggestedConnections />
-        <SeeMoreButton />
-        
       </div>
-
-      {/* right side */}
-      <div className="bg-white flex justify-center">
-        <div className="flex flex-col bg-gray-100 rounded-md border border-gray-300 w-[390px] m-6"> 
-          <h2>This Week:</h2>
-          <p className="rounded-3xl w-36 h-48 bg-blue-200">placeholder for progress bar</p>
-
-          {/* component */}
-          <Assignments 
-            title="Complete"
-            assignments={toCompleteAssignments}
-          />
-
-          <Assignments 
-            title="Upcoming"
-            assignments={upcomingAssignments}
-          />
-
-          <SeeMoreButton />
-
-        </div>
-      </div>
-
-    </div>
+    </>
   );
 };
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import brandName from '../assets/brand-name.png'
+import React from "react";
+import brandName from "../assets/brand-name.png";
 
 const Header = () => {
   return (
-    <div className='pl-12'>
+    <div className="pl-12 bg-white mt-10 mb-20 w-full">
       <img src={brandName} alt="" width={160} />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
